f301e0ad21342e1b002a7fc990d6f99e4228a393
xuweitao@youzan.com <xuweitao@youzan.com>
2020-07-06 21:05:46 +0800

    新增开发模式中间件配置文件

---
+++ config/middlewares.dev.ts
@@ -0,0 +1,17 @@
+import { PureObject } from '../definitions/common';
+import { MiddlewareConfig } from '../definitions/core';
+
+const middlewaresConfig:PureObject<MiddlewareConfig> = {
+    'sunday-nunjucks-assets': {
+        enable: true,
+        priority: 65,
+        options: {
+            redirectMatch: [
+                /hot-update/,
+                /js\/(?:vendors~)?saturday\//
+            ]
+        }
+    }
+}
+
+export = middlewaresConfig;
