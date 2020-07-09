364f802f2aa229053f2170efb00e453a293241f5
xuweitao@youzan.com <xuweitao@youzan.com>
2020-07-09 21:16:15 +0800

    修改路由匹配规则

--- app/controllers/SundayUiController.ts
+++ app/controllers/SundayUiController.ts
@@ -2,13 +2,13 @@ import { Context } from 'koa';
 import { Controller, Route, Get } from '../../plugins/sunday-decorator/app/lib';
 import { BaseApplication } from '../../definitions/core';
 
-@Controller('/ui/:component')
+@Controller('/ui')
 class SundayUiController {
     app: BaseApplication;
     ctx: Context;
     
     @Get
-    @Route()
+    @Route('/:component?/(.*)')
     async getName() {
         this.ctx.setGlobal('title', 'sunday-ui');
         await this.ctx.render('sunday-ui.html');
